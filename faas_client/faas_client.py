61fb00406ab819a4cfb65cfdfbe5c50a822f56a6
Laurent Michel <laurent.michel@cognex.com>
2022-12-14 21:18:39 +0100

    add support for env vars

--- faas_client/faas_client.py
+++ faas_client/faas_client.py
@@ -1,6 +1,7 @@
 import os.path
 import re
 from io import StringIO
+from typing import Dict
 
 import sh
 
@@ -12,45 +13,55 @@ def build_env_options(env):
 
 
 class FaasClient:
-    def __init__(self, gateway_url, gateway_port, username, password):
-        self.__cli = sh.Command("faas-cli")
+    def __init__(
+        self,
+        gateway_url: str,
+        gateway_port: int,
+        username: str,
+        password: str,
+        env: Dict[str, str] = None,
+    ):
+        self._cli = sh.Command("faas-cli")
         self.endpoint = f"{gateway_url}:{gateway_port}"
-        self.__username = username
-        self.__password = password
+        self._username = username
+        self._password = password
+        self._env = env
 
     def login(self):
-        result = self.__cli(
+        result = self._cli(
             "login",
             "-g",
             self.endpoint,
             "--username",
-            self.__username,
+            self._username,
             "--password",
-            self.__password,
+            self._password,
         )
         return result.exit_code
 
     def build(self, path_to_faas_configuration, function_name):
         configuration_filename = os.path.basename(path_to_faas_configuration)
-        result = self.__cli(
+        result = self._cli(
             "build",
             "-f",
             configuration_filename,
             "--filter",
             function_name,
             _cwd=os.path.dirname(path_to_faas_configuration),
+            _env=self._env,
         )
         return result.exit_code
 
     def push(self, path_to_faas_configuration, function_name):
         configuration_filename = os.path.basename(path_to_faas_configuration)
-        result = self.__cli(
+        result = self._cli(
             "push",
             "-f",
             configuration_filename,
             "--filter",
             function_name,
             _cwd=os.path.dirname(path_to_faas_configuration),
+            _env=self._env,
         )
         return result.exit_code
 
@@ -58,7 +69,7 @@ class FaasClient:
         if env is None:
             env = {}
         env_options = build_env_options(env)
-        result = self.__cli(
+        result = self._cli(
             "deploy",
             "-f",
             path_to_faas_configuration,
@@ -72,7 +83,7 @@ class FaasClient:
 
     def is_ready(self, function_name):
         output_buffer = StringIO()
-        result = self.__cli(
+        result = self._cli(
             "describe", function_name, "-g", self.endpoint, _out=output_buffer
         )
         if result.exit_code != 0:
@@ -85,8 +96,11 @@ class FaasClient:
 
 
 class FaasClientFactory:
-    def __init__(self, gateway_port):
-        self.__gateway_port = gateway_port
+    def __init__(self, gateway_port: int, env: Dict[str, str] = None):
+        self._gateway_port = gateway_port
+        self._env = env
 
     def create(self, gateway_url, username, password):
-        return FaasClient(gateway_url, self.__gateway_port, username, password)
+        return FaasClient(
+            gateway_url, self._gateway_port, username, password, self._env
+        )
